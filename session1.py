bf9d83c0d1ab321210df00d4e45b41de98e88050
GitTanish <tanishsaroj6781@gmail.com>
2024-12-08 21:03:25 +0530

    Add session.py

---
+++ session1.py
@@ -0,0 +1,34 @@
+# baby steps
+print("Hello world")
+print("print\nnew line\nnew line",9,7.6,True,sep='!')#seperation which is by default=' '
+# so sep for separation override
+
+#integer
+# 1*10^308 #clocked at this value
+print(1e309) #infinite
+print(8*10000+4737)
+
+#float/decimal
+# 1.7e+308
+print(1.2+4.5)
+
+#Boolean
+print(True)
+print(False)
+
+# String
+print('Rick '+'Sanchez C-137')
+
+#complex
+print(5+7j)
+
+#List -> C-> Array
+print([1,2,3,4,5,6])
+
+#Tuple
+print((1,2,3,4))
+
+#sets
+print({1,2,3,4,5,6})
+
+#Dictionary
