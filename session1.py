6fd955ee85ac3d410a8213a9e13a25849bf10094
GitTanish <tanishsaroj6781@gmail.com>
2024-12-08 21:56:36 +0530

    update 1 session.py

--- session1.py
+++ session1.py
@@ -32,3 +32,33 @@ print((1,2,3,4))
 print({1,2,3,4,5,6})
 
 #Dictionary
+dictionary1 = {'Rick':'Sanchez','Morty':'Smith','Subaru':'Natsuki'} # anti climatic eh
+print(dictionary1)
+
+#type
+type('Peak')
+
+## Variables
+name = 'Android 17'
+print(name)
+
+# Important Points(Very important!) :)
+
+# static vs dynamic typing
+# don't have to tell the data type while creating a Variable, cause it python and interpreter can understand :)
+a =5 # rather than int a =5 (static typing)C,JAVA,C++
+
+# Dynamic Binding
+a = 5
+print(a)
+a = 'nitish'
+print(a)
+#notice no error cause python support dynamic binding i.e variable can hold any data type
+
+a=1
+
+a,b,c = 1,2,3
+print(a,b,c)
+
+x=y=z = 5
+print(x,y,z)
