bb3989b26339d1c7378b62c9b1cf967cf69f6998
GitTanish <tanishsaroj6781@gmail.com>
2024-12-09 22:00:37 +0530

    adding Loops

---
+++ Loops.py
@@ -0,0 +1,30 @@
+#Operators
+
+#Arithmetic
+print(5+6)
+print(1-6)
+print(1/6)
+print(5*2)
+print(5//2) #int division
+print(5**2) # 5^2 #power operator
+
+#relational operators
+print(4>5)
+print(4<5)
+print(4>=5)
+print(4<=5)
+print(4==4)
+print(4!=5)
+
+#Logical Operator
+print(1 and 0)
+print(1 or 0)
+print(not 1)
+
+#BitWise Operator
+print(2 & 3) # bitwise and :- 010&110=010 =>2
+print(2|3)   # bitwise or :- 010 or 110=110 => 3
+print(2 ^ 3) # bitwise xor:- 010 xor 110 =100 => 1
+print(~3)    # bitwise not not(110)=001 #basically 2's compliment
+print(4>>2)  # left shift
+print(4<<2)  # right shift
