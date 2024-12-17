4860da5a779e798f164e4a4114b6b8420d96cc28
GitTanish <tanishsaroj6781@gmail.com>
2024-12-17 23:49:18 +0530

    added args and kwargs

--- functions.py
+++ functions.py
@@ -1,14 +1,61 @@
-def is_even(num):
-  """
-  This function returns if a given number is odd or even
-  input - any valid integer
-  output - odd/even
-  created on - 16th Nov 2022
-  """
-  if type(num) == int:
-    if num % 2 == 0:
-      return 'even'
+# num = int(input("Enter number: "))
+
+def is_even(i):# the i is parameter
+    """checking if the number entered is
+     even or odd"""
+    if type(i) ==int:
+        if i%2==0:
+            return 'even'
+        else:
+            return 'odd'
     else:
-      return 'odd'
-  else:
-    return 'pagal hai kya?'
+        return 'enter an integer value'
+
+
+for i in range(1,11):
+    x = is_even(i)
+    print(x)
+
+print(is_even('hello')) # the hello is argument
+
+# types of argument:
+
+## default argument
+
+#
+def power(a=1,b=1):
+    return a**b
+
+print(power())
+
+## positional arguments
+## key word argument
+
+# args amd kwargs
+# allows us to pass a variable number of non-keyboard arguments to a function
+
+# in this program we have to explicitly define how many parameters are possible
+def multiply(a=1,b=1,c=1):
+    return a*b*c
+
+multiply(1,2)
+
+# by using we can  remove the limited parameters
+def multiply(*args):
+    prod =1
+    for i in args:
+        prod=prod*i
+    return prod
+print(multiply(1,23,9,4,5,9,6,7,8))
+
+print(print.__doc__) # .__docs__ used to summon documentation of the python
+
+# **kwargs
+# **kwargs allows us to pass any number of keyword arguments.
+# Keyword arguments mean that they contain a key-value pair, like a Python dictionary.
+
+def display(**kwargs):
+    for (key,value) in kwargs.items():
+        print(key,'->',value)
+
+display(india='Delhi',china='shanghai',america='washington dc')
